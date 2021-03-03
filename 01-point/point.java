cfae7b9935f724c5bd17f90862978fbb8929b7df
shehryartariq <tm678123@gmail.com>
2021-03-03 13:55:04 -0500

    Done

--- 01-point/point.java
+++ 01-point/point.java
@@ -7,7 +7,7 @@ public class Point{
    private int y;
 
    // constructs a new point at the origin, (0, 0)
-   public point(){
+   public Point(){
      this(0,0); // calls Point(int, int) constructor
    }
 
@@ -33,11 +33,6 @@ public class Point{
      return y;
    }
 
-   // returns a String representation of this point
-   public String toString() {
-     return "(" + x + ", " + y + ")";
-   }
-
    // returns the distance between this Point and (0, 0)
    public double distanceFromOrigin() {
      return Math.sqrt(x * x + y * y);
@@ -48,12 +43,30 @@ public class Point{
      setLocation(x + dx, y + dy);
    }
 
-   // shifts this point's location by the given amount
-   public void translate(int dx, int dy) {
-     setLocation(x + dx, y + dy);
+
+   public void distance(Point point) {
+      return Math.sqrt(Math.pow(this.x - point.getX(), 2) + Math.pow(this.y - point.getY(), 2));
    }
 
-   public void distance
-   public void midpoint
-   public void slope(int poin)
-   public void isCollinear
+   public void midpoint(Point point) {
+     int mid=((this.x + point.getX())/2);
+     int vid=((this.y + point.getY())/2);
+     return new Point(mid,vid);
+   }
+
+   public void slope(Point point){
+     return ((point.getY()-this.y)/(point.getX()-this.x));
+   }
+
+   public void isCollinear(Point point, Point point2){
+     if (this.slope(point) == this.slope(point2)){
+       return true;
+     }
+     return false;
+   }
+
+   // returns a String representation of this point
+   public String toString() {
+     return "(" + x + ", " + y + ")";
+   }
+ }
