9b52e3d2559cf9f784a8538d4db40c9776d1229d
shehryartariq <tm678123@gmail.com>
2021-03-08 18:00:57 -0500

    Done with FRQ's

--- 00-SuperArray/SuperArray.java
+++ 00-SuperArray/SuperArray.java
@@ -1,4 +1,33 @@
 import java.util.*;
-public class SuperArray;{
+public class SuperArray{
+
+  //instance Variables
+
+
+
+
+
+  public void add(int val){
+
+
+  }
+
+  public void add(int index, int val){
+
+
+  }
+  public void grow(int n){
+
+  }
+  public void set(int i, int val){
+
+  }
+  public boolean isEmpty(){
+
+  }
+  public void remove(int index){
+
+  }
+  public String toString()
 
 }
